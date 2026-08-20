e7eba90116ae284b98be4300c7423622a48460f3
Babureddy B N <babureddy009@gmail.com>
2026-08-21 01:37:55 +0530

    Test automatic frontend deployment

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -12,6 +12,7 @@ function MovieList({ onMovieClick }) {
   }, []);
 
   return (
+    <h1>Movie List - CI/CD</h1>
     <ul>
       {movies.map((movie) => (
         <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
