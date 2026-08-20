3528dfe58e6c25362afeac3a9c49854086e0407a
Babureddy B N <babureddy009@gmail.com>
2026-08-21 01:46:13 +0530

    Format frontend JSX

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -1,26 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
 
 function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
-      setMovies(response.data.movies);
-    });
+    axios
+      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
+      .then((response) => {
+        setMovies(response.data.movies);
+      });
   }, []);
 
   return (
     <>
-    <h1>Movie List - CI/CD</h1>
-    <ul>
-      {movies.map((movie) => (
-        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
-          {movie.title}
-        </li>
-      ))}
-    </ul>
+      <h1>Movie List - CI/CD</h1>
+      <ul>
+        {movies.map((movie) => (
+          <li
+            className="movieItem"
+            key={movie.id}
+            onClick={() => onMovieClick(movie)}
+          >
+            {movie.title}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
